cccecc27f5fadbaf55dc5957133f75fee1ba4d14
roryjl-n <rory.lacayon@gmail.com>
2019-10-11 08:16:05 -0700

    Scripts

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+//document.body.style.backgroundColor = "yellow";
+//alert(1+2+3);
+//let age =  25;
+//alert("age = " + age);
+<form>
+<input type=button value="Close Window" onClick="javascript:window.close();">
+</form> 
